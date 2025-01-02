4fb4707a87c355607a4eb6fb1a6ccdb5885526ff
chanmaxsolutions <80768779+chanmaxsolutions@users.noreply.github.com>
2025-01-02 22:38:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Metadata } from 'next';
 import Navigation from '@/components/Navigation';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Tapskal - Digital Marketing Agency',
@@ -20,24 +21,37 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link 
-          rel="icon" 
-          href="https://res.cloudinary.com/dqeujzydw/image/upload/v1735628159/Tapskal-Favicon_s32ijx.svg" 
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/dqeujzydw/image/upload/v1735628159/Tapskal-Favicon_s32ijx.svg"
         />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W889F66K');`}
+        </Script>
       </head>
-      <body 
+      <body
         className={`${generalSans.variable} font-sans bg-light text-dark`}
         suppressHydrationWarning
       >
-        {/* Header */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W889F66K"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Navigation />
-        
-        {/* Page Content */}
+
         <main className="min-h-screen">
           {children}
         </main>
-        
-        {/* Footer */}
+
         <Footer />
       </body>
     </html>
